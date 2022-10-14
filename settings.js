cd84f6e86e6fb45b19595c863fb3b9a4faef8e5b
jasajadibot <113238988+jasajadibot@users.noreply.github.com>
2022-10-14 12:47:36 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,25 +19,25 @@
   }
 
   // Setting Owner
-  global.owner = ['6281575886399','6281575886399','6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-  global.premium = ['6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-  global.ownernomer = '6281575886399' //ubah jadi nomor mu, note tanda ' gausah di hapus!
+  global.owner = ['6285743137623','6285743137623','6285743137623'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+  global.premium = ['6285743137623'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+  global.ownernomer = '6285743137623' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
   // Set Nama Own & Bot
-  global.ownername = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
-  global.botname = 'Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-  global.footer = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+  global.ownername = 'Balz Ofc' //ubah jadi nama mu, note tanda ' gausah di hapus!
+  global.botname = 'Balz Botz-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+  global.footer = 'Balz Ofc' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
   // Set Kebutuhan Button & Kontak
   //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
-  global.youtube = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qE4Bw'
-  global.mygc = 'https://chat.whatsapp.com/CRY23lVKmXB7sCMhHmcwVw'
-  global.myweb = 'https://api-riychdwayne.herokuapp.com'
+  global.youtube = 'https://yandex.com'
+  global.mygc = 'https://yandex.com'
+  global.myweb = 'https://yandex.com'
   global.region = 'indonesia'
 
   // Set Wm
-  global.packname = '© Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-  global.author = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+  global.packname = '© Balz Botz-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+  global.author = 'Bakz' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
   // Set Nama Session
   //gausah di apa² in!
